8e6389cfe524171c4f662cf79c5ecf2915fa5137
Joshua <joshuakdeng@gmail.com>
2017-09-11 00:10:53 -0500

    fix typo

--- UTA_bot.py
+++ UTA_bot.py
@@ -21,7 +21,7 @@ def run_bot(reddit, replied):
     print("getting submissions")
     for submission in reddit.subreddit('UTAustin').new(limit=5):
         if (" uta" in submission.title or " uta" in submission.selftext 
-            or " UTA" in submission.title or " UTA" in submission.selftextor 
+            or " UTA" in submission.title or " UTA" in submission.selftext 
             or "UTA " in submission.title) and submission.id not in replied:
 
             print("string with 'UTA' found in " + submission.id)
